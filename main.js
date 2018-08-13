138cf6ada745179dd9d7ac3b4da423c0a348052e
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2018-08-13 00:41:22 -0400

    remove code in main.js as it is not required to lead the animations

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-//$(document).ready(function() {
-//	alert("hi")
+//window.addEventListener('load', function() {
+//	document.querySelector("body").classList.add("play");
 //});
-
-window.addEventListener('load', function() {
-	document.querySelector("body").classList.add("play");
-});
